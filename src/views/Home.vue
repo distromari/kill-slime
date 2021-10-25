7a4383f866e1f310fbb97cc1bcc309ecaab9ae33
distromari <mariheloisa010@gmail.com>
2021-10-25 15:44:44 -0300

    Organizing visual components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,67 +1,74 @@
 <template>
-  <div class="rpgui-pixelated">
-    <div class="panel scores">
-      <div class="score">
-        <h1>Player</h1>
-        <img src="./assets/gifs/sprite-1.gif" alt="">
-        <div class="life-bar rpgui-progress">
-          <div 
-            class="life"
-            :class="{ danger: playerLife < 20 }"
-            :style="{ width: playerLife + '%'}"
-          >
+  <section class="container">
+    <div class="rpgui-pixelated">
+      <div>
+        <h1 class="title">Kill slime</h1>
+      </div>
+      <div class="panel">
+        <div class="scores">
+          <div class="score">
+            <h1>Player</h1>
+            <img src="./assets/gifs/sprite-1.gif" alt="Sprite 1">
+            <div class="life-bar rpgui-progress rpgui-progress-track">
+              <div 
+                class="rpgui-progress-fill green"
+                :class="{ danger: playerLife < 20 }"
+                :style="{ width: playerLife + '%'}"
+              >
+              </div>
+            </div>
+            <p>{{ playerLife }}%</p>
+          </div>
+          <div class="score">
+            <h1>Slime</h1>
+            <img src="./assets/gifs/sprite-2.gif" alt="Sprite 2">
+            <div class="life-bar rpgui-progress rpgui-progress-track">
+              <div 
+                class="rpgui-progress-fill red"
+                :class="{ danger: slimeLife < 20 }"
+                :style="{ width: slimeLife + '%'}"
+              >
+              </div>
+            </div>
+            <p>{{ slimeLife }}%</p>
           </div>
         </div>
-        <div>{{ playerLife }}%</div>
-      </div>
-      <div class="score">
-        <h1>Slime</h1>
-        <img src="./assets/gifs/sprite-2.gif" alt="">
-        <div class="life-bar rpgui-progress">
-          <div 
-            class="life"
-            :class="{ danger: slimeLife < 20 }"
-            :style="{ width: slimeLife + '%'}"
-          >
-          </div>
-          <div>{{ slimeLife }}%</div>
+        <div class="rpgui-container framed-golden buttons">
+          <template v-if="running">
+            <div class="btn">
+              <button class="btn attack rpgui-icon sword" @click="attack(false)"></button>
+              <p>Attack</p>
+            </div>
+            <div class="btn">
+              <button class="btn especial-attack rpgui-icon sword" @click="attack(true)"></button>
+              <p>Especial attack</p>
+            </div>
+            <div class="btn">
+              <button class="rpgui-icon potion-red" @click="healPlayer()"></button>
+              <p>Heal</p>
+            </div>
+            <div class="btn">
+              <button @click="running = false" class="btn rpgui-icon shield"></button>
+              <p>Give up / Play again</p>
+            </div>
+          </template>
+          <button v-else @click="startGame" class="rpgui-button">Play!</button>
         </div>
       </div>
+      <div v-if="hasResult" class="panel result">
+        <h3 v-if="slimeLife == 0" class="win">You won :)</h3>
+        <h3 v-else class="lose">You lose :'(</h3>
+      </div>
+      <div v-if="logs.length" class="panel logs">
+        <ul>
+          <li :v-for="log in logs" :class="log.cls" class="log">{{ log.text }}</li>
+        </ul>
+      </div>
     </div>
-    <div v-if="hasResult" class="panel result">
-      <div v-if="slimeLife == 0" class="win">You won :)</div>
-      <div v-else class="lose">You lose :'(</div>
-    </div>
-    <div class="panel buttons rpgui-container framed">
-      <template v-if="running">
-        <div>
-          <button class="btn attack rpgui-icon sword" @click="attack(false)"></button>
-          <p>Attack</p>
-        </div>
-        <div>
-          <button class="btn especial-attack rpgui-icon sword" @click="attack(true)"></button>
-          <p>Especial attack</p>
-        </div>
-        <div>
-          <button class="rpgui-icon potion-red"></button>
-          <p>Heal</p>
-        </div>
-        <div>
-          <button @click="running = false" class="btn rpgui-icon shield"></button>
-          <p>Give up</p>
-        </div>
-      </template>
-      <button v-else @click="startGame" class="rpgui-button">Play!</button>
-    </div>
-    <div v-if="logs.length" class="panel logs">
-      <ul>
-        <li :v-for="log in logs" class="log">{{ log.text }}</li>
-      </ul>
-    </div>
-  </div>
+  </section>
 </template>
 
-<script>
+<script> 
 export default {
   data () {
     return {
@@ -80,7 +87,7 @@ export default {
     startGame() {
       this.running = true
       this.playerLife = 100
-      this.monsterLife = 100
+      this.slimeLife = 100
       this.log = []
     },
     attack(especial) {
@@ -95,9 +102,8 @@ export default {
       this[prop] = Math.max(this[prop] - hurt, 0)
       this.registerLog(`${source} attacks ${target} with ${hurt}`, cls)
     },
-    healAndHurt() {
+    healPlayer() {
       this.heal(10, 15)
-      this.hurt('playerLife', 7 , 12, false, 'Slime', 'Player', 'slime')
     },
     heal(min, max) {
       const heal = this.getRandom(min, max)
